ba94edd0ce292c9424ed41e18b63e3473a5fcab7
807119680 <807119680@qq.com>
2018-09-28 20:31:57 +0800

    Initial Commit

---
+++ testswift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testswift
+//
+//  Created by s20171106168 on 2018/9/28.
+//  Copyright © 2018年 s20171106168. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
